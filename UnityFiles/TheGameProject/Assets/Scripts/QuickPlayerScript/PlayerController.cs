3b2995d178d1b17a5c754370942fa50ea689006c
DragstylerNL <dragstylernl@gmail.com>
2018-05-15 13:47:41 +0200

    quick upload

---
+++ UnityFiles/TheGameProject/Assets/Scripts/QuickPlayerScript/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    /*
+     * Playercontroller zorgt voor het behandelen van input
+     * PlayerMotor zorgt ervoor dat de megegeven Behandelde input
+     * ook daadwerkelijk wordt 'gedaan'
+     */
+    private PlayerMotor motor;
+
+    // groundCollider zorgt voor detectie van Ground
+    public Transform GroundCollider;
+}
