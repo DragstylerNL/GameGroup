6047e7c891b493e095540576e90fc6a5d91ca94d
DragstylerNL <dragstylernl@gmail.com>
2018-05-17 10:26:45 +0200

    destroy fail

---
+++ UnityFiles/TheGameProject/Assets/destroyOldPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOldPlatform : MonoBehaviour {
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        print("ok");
+        print(other);
+        Destroy(other);
+    }
+}
