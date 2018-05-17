d6ffa5f40d3b384491eed2b0cf810b2ced47d09a
Dexter <itsdexter66@gmail.com>
2018-05-17 09:35:54 +0200

    Power-up demo toegevoegd

--- UnityFiles/TheGameProject/Assets/Scripts/PowerUpScripts/PowerUp.cs
+++ UnityFiles/TheGameProject/Assets/Scripts/PowerUpScripts/PowerUp.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour
 {
 
-    private void OnTriggerEnter(Collider other)
+    public float sizeEffect = 1.4f;
+    public float effectDuration = 4f;
+
+    public GameObject pickupEffect;
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) // kijkt of ie de "Player" aanraakt en voert dan Pickup() funtie uit
         {
-            Pickup();
+            StartCoroutine( Pickup(other) );
         }
     }
 
-    void Pickup()
+    IEnumerator Pickup(Collider2D player)
     {
-        Debug.Log("Power-up trigger");
+        //spawns een pickup effect
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //geeft power-up effect aan Player
+        player.transform.localScale *= sizeEffect;
+
+        //zorgt dat de power-up niet blijft maar ontzichbaar wordt
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+
+        //wacht x hoeveelheid secondes
+        yield return new WaitForSeconds(effectDuration);
+
+        //draait het effect op de player terug
+        player.transform.localScale /= sizeEffect;
+
+        //verwijder Power-up van scene
+        Destroy(gameObject);
     }
 }
