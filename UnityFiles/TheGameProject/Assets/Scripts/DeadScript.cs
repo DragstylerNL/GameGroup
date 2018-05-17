55da8d53762a2d14f975837b0f763a4bd5ad9012
DragstylerNL <dragstylernl@gmail.com>
2018-05-17 13:54:19 +0200

    destroy succes now

--- UnityFiles/TheGameProject/Assets/Scripts/DeadScript.cs
+++ UnityFiles/TheGameProject/Assets/Scripts/DeadScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class destroyOldPlatform : MonoBehaviour {
+public class DeadScript : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D other)
     {
---
+++ UnityFiles/TheGameProject/Assets/Scripts/DeadTimer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadTimer : MonoBehaviour {
+
+    private float Timer = 0;
+    private float EndOffTheLine = 7;
+	
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime;
+        if (Timer > EndOffTheLine)
+            Destroy(this.gameObject);
+	}
+}
