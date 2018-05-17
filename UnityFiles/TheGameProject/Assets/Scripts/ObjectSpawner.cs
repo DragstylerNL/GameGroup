bbf41f4e5b6b7eb0c15140765760ba39864556ba
DragstylerNL <dragstylernl@gmail.com>
2018-05-17 09:49:54 +0200

    random generation & jump
    rnd generation werkt en doet het

---
+++ UnityFiles/TheGameProject/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour {
+
+    // de 3 posities (hoogtes) waar prefabs kunnen spawnen
+    public Transform[] spawnPositions;
+
+    //alle spawnable objects
+    public Object[] prefabs;
+
+    // timer voordat de nieuwe platform wordt gemaakt
+    [Range(0.5f, 5f)]
+    public float timer;
+    // tijd bijhouder
+    private float timePast;
+
+
+    void Start () {
+
+    }
+	
+
+	void Update () {
+        // voeg vergaande tijd toe
+        timePast += Time.deltaTime;
+
+        // kijk of er genoeg tijd is vergaan om een nieuwe prefab te spawnen
+        if(timePast > timer)
+        {
+            // random aantal platforms 
+            int _rnd = Mathf.CeilToInt(Random.value * 3);
+
+            for(int i = 0; i < _rnd; i++)
+            {
+                int _rndPrefab = Mathf.CeilToInt(Random.value * prefabs.Length) - 1;
+                Instantiate(prefabs[_rndPrefab], spawnPositions[i]);
+            }
+
+            // timePast weer naar 0
+            timePast = 0;
+        }
+
+	}
+}
