34f9d44cef574082e9e1a9df87921b8176dc94e7
Dexter <itsdexter66@gmail.com>
2018-05-18 12:03:53 +0200

    StartMenu begin

---
+++ UnityFiles/TheGameProject/Assets/StartMenu/ButtonTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonTest : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scene");
+    }
+
+    //public void QuitGame()
+    //{
+    //    Debug.Log("QUIT!");
+    //    Application.Quit();
+    //}
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void ScoreBoard()
+    {
+        SceneManager.LoadScene("ScoreBoards");
+    }
+}
