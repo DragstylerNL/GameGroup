5661970a7510e7f11e6040617cfc157790d31e77
AlexJanson <alex@hoofddorp.nl>
2018-05-25 10:24:23 +0200

    +playerJump anim

---
+++ UnityFiles/TheGameProject/Assets/Scripts/AnimationScripts/PlayerAnimController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimController : MonoBehaviour {
+
+    Animator anim;
+    PlayerController playerController;
+    bool isJumping;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(playerController._jumpInput) {
+            isJumping = true;
+        }
+        if(playerController.grounded) {
+            isJumping = false;
+        }
+
+        anim.SetBool("isJumping", isJumping);
+		
+	}
+}
