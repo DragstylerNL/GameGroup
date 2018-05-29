9cba9756952d4d19d37be9ee2ed7e05f00f714ba
Dexter <itsdexter66@gmail.com>
2018-05-29 16:57:32 +0200

    power up verder gewerkt

--- UnityFiles/TheGameProject/Assets/Scripts/PowerUpScripts/PowerUpBiggerSize.cs
+++ UnityFiles/TheGameProject/Assets/Scripts/PowerUpScripts/PowerUpBiggerSize.cs
@@ -5,36 +5,56 @@ using UnityEngine;
 public class PowerUpBiggerSize : MonoBehaviour
 {
 
-    public float sizeEffect = 1.4f;
+    //public float sizeEffect = 1.4f;
     public float effectDuration = 4f;
+    public float speed = 10;
+
+    private Transform ts;
+    private PlayerAnimController animController;
+    public GameObject speler;
 
     public GameObject pickupEffect;
 
-    private void OnTriggerEnter2D(Collider2D other)
+	void Start()
+	{
+        ts = GetComponent<Transform>();
+        animController = speler.GetComponent<PlayerAnimController>();
+	}
+
+	void Update()
+	{
+        ts.position -= Vector3.right * speed * Time.deltaTime;
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) // kijkt of ie de "Player" aanraakt en voert dan Pickup() funtie uit
         {
+            animController.animate("hasRocketBoots", true);
+            Time.timeScale = 0;
             StartCoroutine( Pickup(other) );
         }
     }
 
     IEnumerator Pickup(Collider2D player)
     {
+        
         //spawns een pickup effect
         Instantiate(pickupEffect, transform.position, transform.rotation);
 
         //geeft power-up effect aan Player
-        player.transform.localScale *= sizeEffect;
+        //player.transform.localScale *= sizeEffect;
 
         //zorgt dat de power-up niet blijft maar ontzichbaar wordt
-        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<SpriteRenderer>().enabled = false;
         GetComponent<Collider2D>().enabled = false;
 
         //wacht x hoeveelheid secondes
         yield return new WaitForSeconds(effectDuration);
 
         //draait het effect op de player terug
-        player.transform.localScale /= sizeEffect;
+        //player.transform.localScale /= sizeEffect;
+        animController.animate("hasRocketBoots", false);
 
         //verwijder Power-up van scene
         Destroy(gameObject);
