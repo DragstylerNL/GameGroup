d619f1b838d115b27f4cc9f5aef93bdca4f2d869
DragstylerNL <dragstylernl@gmail.com>
2018-06-20 11:29:40 +0200

    background and other stuff

---
+++ UnityFiles/SpaceLizard/Assets/Scripts/BasicImport/MoveThisWay.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveThisWay : MonoBehaviour {
+
+    public Vector3 Movement;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += Movement * Time.deltaTime;
+	}
+}
