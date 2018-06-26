e63b64d82d85ee61f274bd6be8814b9f5d44de14
DragstylerNL <dragstylernl@gmail.com>
2018-06-26 15:00:17 +0200

    efficientie

--- UnityFiles/SpaceLizard/Assets/Scripts/EnvironmentManager.cs
+++ UnityFiles/SpaceLizard/Assets/Scripts/EnvironmentManager.cs
@@ -18,8 +18,7 @@ public class EnvironmentManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (ChangeNow) { SetNewEnvironment(); }
-        ChangeNow = false;
+        if (ChangeNow) { SetNewEnvironment(); ChangeNow = false; }
     }
 
     public void SetNewEnvironment()
